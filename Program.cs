29747a2fbfcc65d14ad8b257763831cc588a4c82
Andrew Ford <aford0040@gmail.com>
2015-05-16 09:31:55 -0400

    Initial Commit
    Files that are obfuscated. The App.config is not obfuscated

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿                                                                     using System;namespace
+                                                       ObfuscationExperiment         {/// <summary>We're
+                                                ///  gonna try and                         obfuscate some code here.
+                                            ///..commentless                                                </summary>
+                                           class                                                                Program
+                                        {static                                                                  void Main
+                                      (string[]                                                                     args)
+                                     {_1_                                                                          ._1()
+                                     ;_1_                       .                             _e.                   O();
+                                     _1_                                                                           .P__P()
+                                     ; Console                                                                    .ReadLine();
+                                     }}public                                                                       class
+                                      _1_{                                   public                                static
+                                        void                                                                       _1()
+                                      {Console                                                                      .WriteLine
+                                       ("    "+                                                                    " _____  "+
+                                     "\n   /   "+                                                                 "   \\ \n "+
+                                        " /     "+                                                               "    \\");
+                                      }public class              _e                             {               public static   
+                                        void O()                 {Console                   .WriteLine          (" |   .   "+
+                                         ".   | \n |           |\n |    /"+              "      |\n |  "+       "         |");
+                                          }}public static                    void P__P()                       {Console.
+                                            WriteLine(" | "+                                          ".       . "+
+                                                     "|\n |  .     . "+                     " |\n  \\   ...  "+
+                                                                     " / \n    \\ ___ /");}}}
+            
+        
+            
+        
+    
+                                                                                  
+                                                                                
+                                                                                       
+                                                                                    
+                                                                                        
+                                                                                    
+                                                                                    
+                                                                                    
+                                                                                           
+                                                                                        
+                                                                                            
+                                                                                        
+                                                                                    
+                                                                                       
+                                                                                    
+                                                                                        
+                                                                                    
+                                                                                
+
